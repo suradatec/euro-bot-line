422c7d7ed68875383f0d71b8f0e24e5f689c3e0d
suradatec <32408976+suradatec@users.noreply.github.com>
2017-09-30 15:35:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,11 +40,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
 //Get the file
-$content = file_get_contents("http://www.google.co.in/intl/en_com/images/srpr/logo1w.png");
+//$content = file_get_contents("http://www.google.co.in/intl/en_com/images/srpr/logo1w.png");
 //Store in the filesystem.
-$fp = fopen("/location/to/save/image.jpg", "w");
-  $arrPostData['messages'][0]['text'] = fwrite($fp, $content);
-fclose($fp); 
+//$fp = fopen("/location/to/save/image.jpg", "w");
+  $arrPostData['messages'][0]['text'] = "http://www.google.co.in/intl/en_com/images/srpr/logo1w.png";
+//fclose($fp); 
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
